602877489e06d267eb7e7b5b58111b974506f5a5
Salvatore <mammoliti.sam@gmail.com>
2024-04-20 14:00:16 -0400

    Added graphql and apolloserver packages. Implement the Apollo Server and apply it to tExpress server as middleware.

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,21 +1,43 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+// const express = require('express');
+// const path = require('path');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server-express4');
+const { authMiddleware} = require('./utils/auth');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const { typeDefs, resolvers } = require('./schemas');
+// const db = require('./config/connection');
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+const server = new ApolloServer({
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  typeDefs,
+  resolvers,
+});
+
+const startApolloServer = async () => {
+  await server.start();
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on your ${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
+});
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+startApolloServer();
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # areca-guppyana
 MERN stack Google Books API search engine utilizing GraphQL API, developed with Apollo Server
+
+<!-- pair program sahil -->
+<!-- insert meta tag thing  -->
