4db13acb088613ccca2dbb57d51939e14876a814
Salvatore <mammoliti.sam@gmail.com>
2024-04-21 13:11:14 -0400

    added mutations to login user, add user, add book and remove book

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,66 @@
+// import the gql function from the Apollo Client library
+import { gql } from "@apollo/client";
+
+// mutation to login a user
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// mutation to add a user
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// mutation to save a book to a user's account
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookInput: BookInput!) {
+    saveBook(bookInput: $bookInput) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+// mutation to remove a book from a user's account
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
