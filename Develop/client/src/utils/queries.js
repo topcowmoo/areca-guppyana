2c112187489f433ff7f78ba89c5c3f4beae86f2e
Salvatore <mammoliti.sam@gmail.com>
2024-04-20 22:19:26 -0400

    imported gql and defined query to get  user information

---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,21 @@
+// import gql from apollo/client to define GraphQL queries
+import { gql } from "@apollo/client";
+
+// define GraphQL query to get user information
+export const GET_ME = gql`
+  query getMe {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
